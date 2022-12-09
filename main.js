83417fa641c4822ba6a423fb0123304a7f0bfdd1
aidemirchik <awirumy14@gmail.com>
2022-12-09 11:19:49 +0600

    dopDz2.2.

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-const lang = ['html', 'css', 'js']
-console.log(lang[0]);
+let num = prompt('Ввудите число от 0 до 4')
+
+const lang = ['html', 'css', 'js', 'c++', 'c#']
+// consolelog(lang[0]);
+switch (num) {
+    case '0':
+        alert(console.log(lang[0]));
+        break;
+    case '1':
+        alert(console.log(lang[1]));
+        break;
+    case '2':
+        alert(console.log(lang[2]));
+        break;
+    case '3':
+        alert(console.log(lang[3]));
+        break;
+    case '4':
+        alert(console.log(lang[4]));
+        break;
+    default:
+        alert('НЕ правильно введено число');
+}
